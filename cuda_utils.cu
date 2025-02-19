a662d03b3d51815af73b27ea9dbaac4fe86ac962
Vishal Kalathil <kalathilvishal1@gmail.com>
2025-02-19 21:14:40 +0530

    modified:   .gitignore 	deleted:    cuda_math.cu 	deleted:    cuda_utils.cu 	deleted:    raytracer 	deleted:    triangle.h

--- cuda_utils.cu
+++
@@ -1,9 +0,0 @@
-#include "cuda_utils.h"
-#include <stdio.h>
-
-void checkCudaError(cudaError_t error, const char* file, int line) {
-    if (error != cudaSuccess) {
-        fprintf(stderr, "CUDA error at %s:%d: %s\n", file, line, cudaGetErrorString(error));
-        exit(EXIT_FAILURE);
-    }
-}
